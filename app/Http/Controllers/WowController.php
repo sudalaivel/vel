9719bb917ebb0dd859d005702f0c2a5d9fb4c9a6
sudalaivel <123751781+sudalaivel@users.noreply.github.com>
2023-01-27 07:14:48 -0800

    Add files via upload

---
+++ app/Http/Controllers/WowController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WowController extends Controller
+{
+    public function gan(){
+        return view("vel");
+    }
+}
